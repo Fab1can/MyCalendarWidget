971c61f5f106cd15281c7273c0196f558ead6cdf
Fab1can <fabiocan@live.com>
2024-03-19 21:26:52 +0100

    Cambiato alarm da repeating ad exact (forse risolto per davvero)

---
+++ app/src/main/java/eu/fab1can/mycalendarwidget/AlarmReceiver.kt
@@ -0,0 +1,13 @@
+package eu.fab1can.mycalendarwidget
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        MyService.start(context)
+    }
+}
